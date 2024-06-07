7fa9289ee9018ea906316e1e707430ae85957268
Benkő-Hadaró Márton <marton.benko@stud.uni-obuda.hu>
2024-06-07 19:51:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NT7PTO
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string alaprajzPath = "alaprajz.txt";
+
+
+            AlaprajzClass alaprajz = new AlaprajzClass(alaprajzPath);
+            int OsszSzabadHely = alaprajz.getCharNum(' ');
+            List<int[]> AP_poz = new List<int[]>();
+            AP_poz = Backtrack(alaprajzPath,alaprajz, AP_poz, 0, OsszSzabadHely);
+            AlaprajzClass alaprajzVegleges = new AlaprajzClass(alaprajzPath);
+            alaprajzVegleges.Kiir(AP_poz);
+            
+        }
+
+        static List<int[]> Backtrack(string path,AlaprajzClass alaprajz, List<int[]> AP_pozi, double ElozoLefedettsege,int OsszSzabadHely)
+        {
+            if(ElozoLefedettsege<0.9)
+            {
+                int[] dim = alaprajz.getDim();
+                int Xrnd = RandomGenerator.rnd.Next(1, dim[0] - 2);
+                int Yrnd = RandomGenerator.rnd.Next(1, dim[1] - 2);
+
+                while(alaprajz.SugarbanVan(AP_pozi,new int[] { Xrnd,Yrnd}))
+                {
+                     Xrnd = RandomGenerator.rnd.Next(1, dim[0] - 2);
+                     Yrnd = RandomGenerator.rnd.Next(1, dim[1] - 2);
+                }
+                alaprajz.APBehelyez(Xrnd, Yrnd);
+
+                foreach (int[] AP in AP_pozi)
+                {
+                    alaprajz.APBehelyez(AP[0], AP[1]);
+                    
+                }
+                double lefedetteseg = alaprajz.getCharNum('0') * 1.0 / OsszSzabadHely  ;
+                
+                
+                if (ElozoLefedettsege > lefedetteseg)
+                {
+                    alaprajz = new AlaprajzClass(path); 
+                    foreach (int[] AP in AP_pozi)
+                    {
+                        alaprajz.APBehelyez(AP[0], AP[1]);
+                    }
+                }
+                else
+                {
+                    AP_pozi.Add(new int[] { Xrnd, Yrnd });
+                    
+                }
+                Backtrack(path,alaprajz, AP_pozi, lefedetteseg, OsszSzabadHely);
+                ;
+            }
+            return AP_pozi;
+        }
+
+
+
+
+
+    }
+}
---
+++ classes.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace NT7PTO
+{
+    
+    static class RandomGenerator
+    {
+        public static Random rnd = new Random();
+    }
+
+    class AlaprajzClass
+    {
+
+        char[,] alaprajz;
+
+        public AlaprajzClass(string file)
+        {
+            alaprajz = Beolvas(file);
+        }
+
+        List<int[]> AP_Poz = new List<int[]>();
+
+        
+
+        public void APBehelyez(int sor, int oszlop)
+        {
+            int r = 15;
+            double rad = (2 * Math.PI);
+            AP_Poz.Add(new int[] { sor, oszlop });
+            
+            for (int i = 1; i < Math.Round(rad * r)+1; i++)
+            {
+                double X = (oszlop + (Math.Sin(rad * (i / (2 * r * Math.PI)))) * r);
+                double Y = (sor + (Math.Cos(rad * (i / (2 * r * Math.PI)))) * r);
+                //Console.WriteLine($"Oszlop:{(int)X} Sor:{(int)Y}");
+
+                try
+                {
+                    if (alaprajz[(int)Y, (int)X] != '#')
+                    {
+                        alaprajz[(int)Y, (int)X] = 'S';
+                    }
+                }
+                catch
+                { }
+                
+                
+            }
+
+            int counterSor = 0;
+            
+            int[] koord2 = new int[2];
+            
+            while (counterSor != 15 )
+            {
+
+                //most csak oszlopra
+                int counterOszlop = 0;
+                
+                try//nagyon fapados, lehetne indexet nezni
+                {
+                    //elso siknegyed
+                    while (
+                         alaprajz[sor - counterOszlop, oszlop + counterSor] != '#' &&
+                         alaprajz[sor - counterOszlop, oszlop + counterSor] != 'S')
+                    {
+                        koord2[0] = sor - counterOszlop;
+                        koord2[1] = oszlop + counterSor;
+                        alaprajz[koord2[0], koord2[1]] = '0';
+                        counterOszlop++;
+                    }
+
+                    //negyedik siknegyed
+                    counterOszlop = 0;
+                    while (
+                         alaprajz[sor + counterOszlop, oszlop + counterSor] != '#' &&
+                         alaprajz[sor + counterOszlop, oszlop + counterSor] != 'S')
+                    {
+                        koord2[0] = sor + counterOszlop;
+                        koord2[1] = oszlop + counterSor;
+                        alaprajz[koord2[0], koord2[1]] = '0';
+                        counterOszlop++;
+                    }
+
+                    //masodik sn
+                    counterOszlop = 0;
+                    while (
+                         alaprajz[sor - counterOszlop, oszlop - (counterSor)] != '#' &&
+                         alaprajz[sor - counterOszlop, oszlop - ( counterSor)] != 'S')
+                    {
+                        koord2[0] = sor - counterOszlop;
+                        koord2[1] = oszlop - (counterSor);
+                        alaprajz[koord2[0], koord2[1]] = '0';
+                        counterOszlop++;
+                    }
+
+
+                    //harmadik
+                    counterOszlop = 0;
+                    while (
+                         alaprajz[sor + counterOszlop, oszlop - counterSor] != '#' &&
+                         alaprajz[sor + counterOszlop, oszlop - counterSor] != 'S')
+                    {
+                        koord2[0] = sor + counterOszlop;
+                        koord2[1] = oszlop - counterSor;
+                        alaprajz[koord2[0], koord2[1]] = '0';
+                        counterOszlop++;
+                    }
+                }
+                catch (Exception exe)
+                {
+
+                }
+                counterSor++;
+
+            }
+
+            for (int i = 1; i < Math.Round(rad * r) + 1; i++)
+            {
+                double X = (oszlop + (Math.Sin(rad * (i / (2 * r * Math.PI)))) * r);
+                double Y = (sor + (Math.Cos(rad * (i / (2 * r * Math.PI)))) * r);
+                //Console.WriteLine($"Oszlop:{(int)X} Sor:{(int)Y}");
+
+                try
+                {
+                    if (alaprajz[(int)Y, (int)X] != '#')
+                    {
+                        alaprajz[(int)Y, (int)X] = '0';
+                    }
+                }
+                catch
+                { }
+
+
+            }
+
+            foreach (int[] AP in AP_Poz)
+            {
+                alaprajz[AP[0], AP[1]] = '*'; //sor az y-koord, oszlop -> x
+            }
+
+        }
+
+        char[,] Beolvas(string file)
+        {
+            string[] be = File.ReadAllLines(file);
+            int sor = be.Length;
+            int oszlop = be[0].Length;
+
+            char[,] alaprajz = new char[sor, oszlop];
+
+            for (int i = 0; i < sor; i++)
+            {
+                for (int j = 0; j < oszlop; j++)
+                {
+                    alaprajz[i, j] = be[i][j];
+                }
+            }
+
+            return alaprajz;
+        }
+
+        public void Kiir()
+        {
+            int sor = alaprajz.GetLength(0);
+            int oszlop = alaprajz.GetLength(1);
+            Console.SetWindowSize(oszlop + 5, sor + 5);
+
+            for (int i = 0; i < sor; i++)
+            {
+                for (int j = 0; j < oszlop; j++)
+                {
+                    Console.Write(alaprajz[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Kiir(List<int[]> AP_poz)
+        {
+            foreach(int[] ap in AP_poz)
+            {
+                alaprajz[ap[0], ap[1]] = '*';
+            }
+            Kiir();
+        }
+
+        public bool SugarbanVan(List<int[]> APk, int[] koord) //false ha sugarban van
+        {
+            bool sugarbanVan = false;
+
+            if(APk==null)
+            {
+                return true;
+            }
+            
+            foreach(int[] ap in APk)
+            {
+                if(!sugarbanVan && (Math.Abs(koord[0] - ap[0]) + Math.Abs(koord[1] - ap[1]) <= 13))
+                {
+                    sugarbanVan = true;
+                }
+            }
+            return sugarbanVan;
+        }
+
+        public int getCharNum(char c)
+        {
+            int lefedett = 0;
+            for (int i = 0; i < alaprajz.GetLength(0); i++)
+            {
+                for (int j= 0; j< alaprajz.GetLength(1); j++)
+                {
+                    if(alaprajz[i, j] == c)
+                    {
+                        lefedett++;
+                    }
+                }
+            }
+            return lefedett;
+        }
+
+        public int[] getDim()
+        {
+            return new int[] { alaprajz.GetLength(0), alaprajz.GetLength(1) };
+        }
+
+    }
+}
